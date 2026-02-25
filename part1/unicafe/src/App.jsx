e1c5b35a13a89118fac5efb7992841739232b0c9
DGChem <denniscatan@gmail.com>
2026-02-25 01:14:47 +0100

    up until 1.5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,31 +18,38 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
-  const total = props.exercises.reduce((a, b) => a + b);
+  const total = props.parts.reduce((a, b) => {
+    return a + b.exercises;
+  }, 0);
+  console.log(total);
   return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
   console.log(course);
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
