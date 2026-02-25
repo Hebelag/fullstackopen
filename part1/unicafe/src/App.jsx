4fc93eef71f664c44a9559d13da38caafa6d7ee7
DGChem <denniscatan@gmail.com>
2026-02-25 22:28:50 +0100

    From 1.6 to 1.12

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,55 +1,131 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+import { useState } from "react";
+
+const FeedbackHeader = () => <h1>give feedback</h1>;
+
+const StatsHeader = () => <h1>statistics</h1>;
+
+const FeedbackButton = ({ name, onClick }) => {
+  return (
+    <button key={name} onClick={onClick}>
+      {name}
+    </button>
+  );
 };
 
-const Part = (props) => {
+const FeedbackColumns = (props) => {
+  const buttons = props.buttonInfo.map((a) => (
+    <FeedbackButton name={a.name} onClick={a.counterFn} />
+  ));
+  return buttons;
+};
+
+const StatisticsLine = ({ name, counter }) => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {name} {counter}
     </p>
   );
 };
 
-const Content = (props) => {
-  const output = props.parts.map((a) => (
-    <Part part={a.name} exercise={a.exercises} />
-  ));
-  return output;
+const Statistics = (props) => {
+  console.log(props);
+  const stats = props.state.map((a) => [a.name, a.counter]);
+  const sumCounter = props.state.reduce((acc, curr) => {
+    return acc + curr.counter;
+  }, 0);
+  stats.push(["all", sumCounter]);
+  return stats;
 };
 
-const Total = (props) => {
-  const total = props.parts.reduce((a, b) => {
-    return a + b.exercises;
+const StatsTable = (props) => {
+  console.log(props);
+  const stats = Statistics(props);
+  const avg = Average(props);
+  const pos = Positive(props);
+  console.log(stats, avg, pos);
+  console.log(stats[0]);
+  return (
+    <table>
+      <tbody>
+        {stats.map((a) => {
+          return (
+            <tr>
+              <td>{a[0]}</td>
+              <td>{a[1]}</td>
+            </tr>
+          );
+        })}
+        <tr>
+          <td>{avg[0]}</td>
+          <td>{avg[1].toFixed(1)}</td>
+        </tr>
+        <tr>
+          <td>{pos[0]}</td>
+          <td>{(pos[1] * 100).toFixed(1)}%</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const Average = (props) => {
+  const sumValues = props.state.reduce((acc, curr) => {
+    return acc + curr.counter * curr.value;
   }, 0);
-  console.log(total);
-  return <p>Number of exercises {total}</p>;
+  console.log(sumValues);
+  const sumCounter = props.state.reduce((acc, curr) => {
+    return acc + curr.counter;
+  }, 0);
+  console.log(sumCounter);
+  const average = sumCounter != 0 ? sumValues / sumCounter : 0;
+  return ["average", average];
+};
+
+const Positive = (props) => {
+  const sumCounter = props.state.reduce((acc, curr) => {
+    return acc + curr.counter;
+  }, 0);
+  let pos = 0;
+
+  for (let x = 0; x < props.state.length; x++) {
+    let feedbackState = props.state[x];
+    console.log(feedbackState);
+    if (feedbackState.name === "good") {
+      pos = feedbackState.counter / sumCounter;
+    }
+  }
+  return ["positive", pos];
 };
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-  console.log(course);
+  // save clicks of each button to its own state
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbackStates = [
+    {
+      name: "good",
+      counter: good,
+      counterFn: () => setGood(good + 1),
+      value: 1,
+    },
+    {
+      name: "neutral",
+      counter: neutral,
+      counterFn: () => setNeutral(neutral + 1),
+      value: 0,
+    },
+    { name: "bad", counter: bad, counterFn: () => setBad(bad + 1), value: -1 },
+  ];
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <FeedbackHeader />
+      <FeedbackColumns buttonInfo={feedbackStates} />
+      <StatsHeader />
+      <StatsTable state={feedbackStates} />
     </div>
   );
 };
