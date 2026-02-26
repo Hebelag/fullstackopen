1fca27584d4c9954d3b0b7bb44dbbcfdf8739a03
DGChem <denniscatan@gmail.com>
2026-02-26 11:40:46 +0100

    copied part1/unicafe from e1c5b35 to part1/courseinfo because I thought it is only a test-exercise and not a submission, sorry!

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  const output = props.parts.map((a) => (
+    <Part part={a.name} exercise={a.exercises} />
+  ));
+  return output;
+};
+
+const Total = (props) => {
+  const total = props.parts.reduce((a, b) => {
+    return a + b.exercises;
+  }, 0);
+  console.log(total);
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+  console.log(course);
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
