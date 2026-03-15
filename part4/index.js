60fe99e06e0eb6891dfff3ffe36d03b3a1790ea9
DGChem <denniscatan@gmail.com>
2026-03-16 00:01:46 +0100

    Exercise 4.2

--- part4/index.js
+++ part4/index.js
@@ -1,39 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-const dns = require('dns')
-dns.setServers(['1.1.1.1'])
+const logger = require('./utils/logger')
 
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
+const app = require('./app')
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
