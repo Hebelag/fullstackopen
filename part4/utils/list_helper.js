aae736de68aa248ed95e88e378775f78558b8888
DGChem <denniscatan@gmail.com>
2026-03-16 12:50:58 +0100

    ESLint configuration and project linting

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,40 +3,40 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    const reducer = (sum, item) => {
-        return sum + item.likes
-    }
-    return blogs.reduce(reducer, 0)
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  return blogs.reduce(reducer, 0)
 }
 
 const favoriteBlog = (blogs) => {
-    return blogs.sort((a,b) => b.likes - a.likes)[0]
+  return blogs.sort((a,b) => b.likes - a.likes)[0]
 }
 
 const mostBlogs = (blogs) => {
-    const blogPerAuthor = blogs.map((a) => {
-        return a.author
-    }).reduce((sum, item) => {
-        sum[item] ? sum[item]++ : sum[item] = 1
-        return sum
-    },{})
-    const bestBlogger = Object.entries(blogPerAuthor).sort((a,b) => b[1]-a[1])[0]
-    return {
-        "author": bestBlogger[0],
-        "blogs": bestBlogger[1]
-    }   
+  const blogPerAuthor = blogs.map((a) => {
+    return a.author
+  }).reduce((sum, item) => {
+    sum[item] ? sum[item]++ : sum[item] = 1
+    return sum
+  },{})
+  const bestBlogger = Object.entries(blogPerAuthor).sort((a,b) => b[1]-a[1])[0]
+  return {
+    'author': bestBlogger[0],
+    'blogs': bestBlogger[1]
+  }
 }
 
 const mostLikes = (blogs) => {
-    const likesPerAuthor = blogs.reduce((sum,item) => {
-        sum[item.author] ? sum[item.author] += item.likes : sum[item.author] = item.likes
-        return sum
-    },{})
-    const bestAuthor = Object.entries(likesPerAuthor).sort((a,b) => b[1]-a[1])[0]
-    return {
-        "author": bestAuthor[0],
-        "likes": bestAuthor[1]
-    }
+  const likesPerAuthor = blogs.reduce((sum,item) => {
+    sum[item.author] ? sum[item.author] += item.likes : sum[item.author] = item.likes
+    return sum
+  },{})
+  const bestAuthor = Object.entries(likesPerAuthor).sort((a,b) => b[1]-a[1])[0]
+  return {
+    'author': bestAuthor[0],
+    'likes': bestAuthor[1]
+  }
 }
 
 module.exports = {
