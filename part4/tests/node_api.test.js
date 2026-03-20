95d9199826375975fadc9394c02d2d0575aed6a2
DGChem <denniscatan@gmail.com>
2026-03-20 01:25:52 +0100

    refactored node_api tests such that the tests are not dependent on db state. using beforeEach and After

--- part4/tests/node_api.test.js
+++ part4/tests/node_api.test.js
@@ -1,10 +1,40 @@
-const { test, after } = require('node:test')
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const assert = require('node:assert')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+/*
+title: String,
+  author: String,
+  url: String,
+  likes: Number,
+*/
+const initialBlogs = [
+  {
+    title: 'Monster Energy White',
+    author: 'Monster',
+    url: 'https://monster.energy/white',
+    likes: 666
+  },
+  {
+    title: 'Monster Energy Pink',
+    author: 'Monster',
+    url: 'https://monster.energy/pink',
+    likes: 666
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogposts are returned as json', async () => {
   await api
@@ -23,7 +53,7 @@ test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs')
 
   const contents = response.body.map(e => e.author)
-  assert.strictEqual(contents.includes('Monster Energy'), true)
+  assert.strictEqual(contents.includes('Monster'), true)
 })
 
 after(async () => {
