0ee96d32b15a9a9172dd68c8af3adda949d38000
DGChem <denniscatan@gmail.com>
2026-03-27 13:39:59 +0100

    First Insight into Redux

---
+++ part6/redux/src/main.jsx
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import noteReducer from './reducers/noteReducer'
+
+
+const store = createStore(noteReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>{store.getState()}</div>
+      <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>
+        plus
+      </button>
+      <button onClick={() => store.dispatch({ type: 'DECREMENT' })}>
+        minus
+      </button>
+      <button onClick={() => store.dispatch({ type: 'ZERO' })}>
+        zero
+      </button>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
