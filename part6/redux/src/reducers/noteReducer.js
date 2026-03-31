8962d67cea1361e8cf7b8d0dbf43c7e648dc88a9
DGChem <denniscatan@gmail.com>
2026-04-01 01:57:34 +0200

    json-server an redux thunk

--- part6/redux/src/reducers/noteReducer.js
+++ part6/redux/src/reducers/noteReducer.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
@@ -24,6 +23,23 @@ const noteSlice = createSlice({
   }
 })
 
-export const { createNote, toggleImportanceOf, setNotes } = noteSlice.actions
+const { createNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const appendNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content)
+    dispatch(createNote(newNote))
+  }
+
+}
+
+export const { toggleImportanceOf } = noteSlice.actions
 
 export default noteSlice.reducer
